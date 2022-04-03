327eae686604ecb9d84f46fd6d6f5e61f835d71a
HeartlessMusician <102092470+HeartlessMusician@users.noreply.github.com>
2022-04-03 18:01:55 +0300

    Add files via upload

---
+++ ProjectTask_2/EntityMapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProgectTask_2
+{
+    public class EntityMapper
+    {
+        public Dictionary<int, List<Entity>> MapIntoDictionary(List<Entity> entities)
+        {
+            var dictionary = new Dictionary<int, List<Entity>>();
+
+            foreach (var entity in entities)
+            {
+                dictionary.TryAdd(entity.ParentId, entities
+                    .Where(e => e.ParentId == entity.ParentId)
+                    .ToList());
+            }
+
+            return dictionary;
+        }
+    }
+}
---
+++ ProjectTask_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProgectTask_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var entities = new List<Entity>()
+            {
+                new Entity { Id = 1, ParentId = 0, Name = "Root entity"},
+                new Entity { Id = 2, ParentId = 1, Name = "Child of 1 entity"},
+                new Entity { Id = 3, ParentId = 1, Name = "Child of 1 entity"},
+                new Entity { Id = 4, ParentId = 2, Name = "Child of 2 entity"},
+                new Entity { Id = 5, ParentId = 4, Name = "Child of 4 entity"}
+            };
+
+            var mapper = new EntityMapper();
+
+            var dictionary = mapper.MapIntoDictionary(entities);
+        }
+    }
+} 
